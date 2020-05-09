e70c3b19d48698ab8be92ad7f8d9c8d5308be32e
alexlecco <alexlecco24@gmail.com>
2020-05-09 14:03:23 -0300

    create Results component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/Main';
+import Results from './components/Results';
 
 export default class App extends Component {
   constructor(props) {
---
+++ src/components/Results.js
@@ -0,0 +1,60 @@
+import React, { Component, Fragment } from 'react';
+import logo from '../logo.png';
+import { Layout, Row, Col, Input } from 'antd';
+
+import ProductCard from './ProductCard';
+
+const { Header, Footer, Sider, Content } = Layout;
+const { Search } = Input;
+
+
+export default class Results extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { userName, products } = this.props;
+
+        return(
+            <Layout>
+                <Header className="header">
+                    <Row>
+                        <Col xs={{ span: 5 }} lg={{ span: 3 }}>
+                            <img src={logo} className="header-logo" alt="logo" />
+                        </Col>
+                        <Col xs={{ span: 19 }} lg={{ span: 16 }}>
+                            <div className="header-search">
+                                <Search
+                                    placeholder="¿Que queres comprar?"
+                                    onSearch={value => console.log(value)}
+                                    enterButton
+                                />
+                            </div>
+                        </Col>
+                        <Col xs={{ span: 0 }} lg={{ span: 5 }}>
+                            <div className="header-greetings">Bienvenido {userName}</div>
+                        </Col>
+                    </Row>
+                </Header>
+
+                <Content className="content">
+                    <p> Resultados la de busqueda </p>
+                    <Row>
+                        {
+                            products.map(prod => (
+                                <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+                                    <ProductCard key={prod.id} product={prod} />
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                </Content>
+ 
+                <Footer className="footer">
+                    Footer
+                </Footer>
+            </Layout>
+        )
+    }
+}
