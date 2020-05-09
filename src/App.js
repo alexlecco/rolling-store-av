f30603f2ac585df3420317b136a1069af373173f
alexlecco <alexlecco24@gmail.com>
2020-05-09 17:55:57 -0300

    improve header style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Main from './components/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Main />
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Alex'
+    }
+  }
 
-export default App;
+  render() {
+    const { userName } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-container">
+          <Main userName={userName} />
+        </header>
+      </div>
+    );
+  }
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
+import { Layout, Input, Row, Col } from 'antd';
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
 export default class Main extends Component {
     render() {
+        const { userName } = this.props;
+
         return(
-            <div>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p> Edit <code>src/App.js</code> and save to reload.</p>
-            </div>
+            <Layout>
+                <Header className="header">
+                    <Row>
+                        <Col xs={{ span:5 }} lg={{ span:3 }}>
+                            <img src={logo} className="header-logo" alt="logo" />
+                        </Col>
+                        <Col xs={{ span:19 }} lg={{ span:16 }}>
+                            <div className="header-search">
+                                <Search
+                                    placeholder="¿Que querés comprar?"
+                                    onSearch={ value => console.log(value) }
+                                    enterButton
+                                />
+                            </div>
+                        </Col>
+                        <Col xs={{ span:0 }} lg={{ span:5 }}>
+                            <div className="header-greetings">Bienvenido {userName}</div>
+                        </Col>
+                    </Row>
+                </Header>
+
+                <Content className="content">
+                    <p> Edit <code>src/App.js</code> and save to reload.</p>
+                </Content>
+
+                <Footer className="footer">
+                    Footer
+                </Footer>
+            </Layout>
         );
     }
 }
