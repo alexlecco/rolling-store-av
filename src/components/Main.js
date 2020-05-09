d231e2294941f7db1412ffc49e6fa0f47a1cbbbe
alexlecco <alexlecco24@gmail.com>
2020-05-09 12:46:18 -0300

    organize space in header

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import logo from '../logo.png';
-import { Layout, Input } from 'antd';
+import { Layout, Input, Row, Col } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 const { Search } = Input;
 
@@ -15,12 +15,23 @@ export default class Main extends Component {
         return(
             <Layout>
                 <Header className="header">
-                    Header
+                    <Row>
+                        <Col xs={{ span: 5 }} lg={{ span: 3 }}>
+                            <img src={logo} className="header-logo" alt="logo" />
+                        </Col>
+                        <Col xs={{ span: 19 }} lg={{ span: 16 }}>
+                            <div className="header-search">Buscador</div>
+                        </Col>
+                        <Col xs={{ span: 0 }} lg={{ span: 5 }}>
+                            <div className="header-greetings">Saludo</div>
+                        </Col>
+                    </Row>
                 </Header>
+
                 <Content className="content">
-                    <img src={logo} className="App-logo" alt="logo" />
                     <p> Edit <code>src/App.js</code> and save to reload. </p>
                 </Content>
+ 
                 <Footer className="footer">
                     Footer
                 </Footer>
