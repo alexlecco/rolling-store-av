c7163308de8eff48e5779c65e7fd0877b3e3c231
alexlecco <alexlecco24@gmail.com>
2020-05-23 02:00:15 -0300

    implement empty state flow

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Layout, Row, Col } from 'antd';
 import ProductCard from '../components/ProductCard';
 const { Content } = Layout;
 
 export default class Results extends Component {
-    render() {
-        const { results } = this.props;
+  render() {
+    const { results } = this.props;
 
-        return(
-            <Layout>
-                <Content className="content">
-                    <p> Resultados la de busqueda </p>
-                    <Row>
-                        {
-                        results.map(resul => (
-                            <Col xs={{ span: 24 }} lg={{ span: 8 }}>
-                                <ProductCard key={resul.id} product={resul} />
-                            </Col>
-                        ))
-                        }
-                    </Row>
-                </Content>
-            </Layout>
-        )
-    }
+    return(
+      <Layout>
+        <Content className="content">
+        {
+          results.length > 0 ? (
+            <Fragment>
+              <p> Resultados la de busqueda </p>
+              <Row>
+                {
+                results.map(resul => (
+                  <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+                    <ProductCard key={resul.id} product={resul} />
+                  </Col>
+                ))
+                }
+              </Row>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <p> No se encontraron resultados </p>
+            </Fragment>
+          )
+        }
+        </Content>
+      </Layout>
+    )
+  }
 }
