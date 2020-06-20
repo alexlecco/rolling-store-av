c1d7a7d7afab1776ef8cdc6dc5365a020d3c898d
alexlecco <alexlecco24@gmail.com>
2020-06-20 10:32:53 -0300

    minimal changes

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -3,9 +3,9 @@ import { Layout } from 'antd';
 const { Footer } = Layout;
 
 const CustomFooter = () => (
-    <Footer className="footer">
-        Footer
-    </Footer>
+	<Footer className="footer">
+		Rolling Store 2020
+	</Footer>
 )
 
 export default CustomFooter
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -9,14 +9,13 @@ const { Content } = Layout
 
 class Product extends Component {
   render() {
-    const { id } = this.props.match.params
     const { product } = this.props.location.state
     const { addToCart } = this.props
 
     return(
       <Layout>
         <Content className="content">
-          <p> Información del producto id: {id} </p>
+          <p> Información del producto </p>
           <ProductInfo
             product={product}
             onAddToCartClicked={() => addToCart(product.id)}
